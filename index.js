a3fd81cf780e0c69b3028c9b086d7e6b2df5c768
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-11-26 21:37:10 +0100

    add license back

--- index.js
+++ index.js
@@ -45,9 +45,32 @@ async function updateGist(stats) {
 
   Helper Project to display wakabox stats for github profile pages.`;
 
-  console.log(`${front} \n # Preview \n ${lines.join("\n")}`);
+  const back = `# Lincese \n
+  MIT License
 
+  Copyright (c) 2020 Qifan Yang
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.`;
   if (lines.length == 0) return;
+
+  const wake_content = lines.join("\n");
+  console.log(`${front} \n # Preview \n ${wake_content}`);
 
   try {
     // Get original filename to update that same file
